a608031f2d58ce2612363df0a52a6c3212308fe1
leticia-nascimento-11a 11a <leticia.conce@eaportal.org>
2025-06-02 00:07:28 +0000

    adição da funcionalidade de guardar a lista no localStorage

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const items = []
+let items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
@@ -45,7 +45,10 @@ function showItemsList() {
             </div>
             `
     })
+
+     localStorage.setItem("items", JSON.stringify(items))
 }
+
 
 function checkItem(itemName) {
     const item = items.find((item) => item.name === itemName)
@@ -73,3 +76,14 @@ function removeItem(itemName) {
 function addHideWarningClass(){
     document.querySelector(".warning").classList.add("hide-warning")
 }
+
+function verifyLocalStorageItems(){
+    const localStorageItems = localStorage.getItem("items")
+
+    if(localStorageItems) {
+        items = JSON.parse(localStorageItems)
+        showItemsList()
+    }
+}
+
+verifyLocalStorageItems()
